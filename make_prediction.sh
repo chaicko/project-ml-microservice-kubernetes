50dcb09d500392bc2b70bdf760dec76b9f9ad4e1
Alejandro Javier Schwoykoski <alejandros@arista.com>
2020-08-04 22:40:43 +1000

    use project file in make_prediction

--- make_prediction.sh
+++ make_prediction.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
+source ./project.conf
 
-PORT=8000
-echo "Port: $PORT"
+echo "Port: $HOST_PORT"
 
 # POST method predict
-curl -d '{  
-   "CHAS":{  
+curl -d '{
+   "CHAS":{
       "0":0
    },
-   "RM":{  
+   "RM":{
       "0":6.575
    },
-   "TAX":{  
+   "TAX":{
       "0":296.0
    },
-   "PTRATIO":{  
+   "PTRATIO":{
       "0":15.3
    },
-   "B":{  
+   "B":{
       "0":396.9
    },
-   "LSTAT":{  
+   "LSTAT":{
       "0":4.98
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+     -X POST http://localhost:$HOST_PORT/predict
