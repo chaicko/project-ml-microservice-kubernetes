616b6f7636afd7283022e693bde505d1f008150f
Alejandro Javier Schwoykoski <alejandros@arista.com>
2020-08-04 22:40:24 +1000

    done with run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-
-## Complete the following steps to get Docker running locally
+source ./project.conf
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t $PROJECT .
 
-# Step 2: 
+# Step 2:
 # List docker images
+docker image ls
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run -d -p $HOST_PORT:$CONTAINER_PORT $PROJECT
