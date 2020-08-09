1ac834be8ccafbeaa137b41f0ce76bef48a191f6
Alejandro Javier Schwoykoski <alejandros@arista.com>
2020-08-09 12:14:02 +1000

    use utilities in make_prediction

--- make_prediction.sh
+++ make_prediction.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
-source ./project.conf
+source utils.sh
 
 echo "Port: $HOST_PORT"
 
-# POST method predict
-curl -d '{
-   "CHAS":{
-      "0":0
-   },
-   "RM":{
-      "0":6.575
-   },
-   "TAX":{
-      "0":296.0
-   },
-   "PTRATIO":{
-      "0":15.3
-   },
-   "B":{
-      "0":396.9
-   },
-   "LSTAT":{
-      "0":4.98
-   }
-}'\
-     -H "Content-Type: application/json" \
-     -X POST http://localhost:$HOST_PORT/predict
+# Call predict utility
+predict 0 6.575 296.0 15.3 396.9 4.98
