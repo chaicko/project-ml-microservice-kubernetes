aa3c1ea3208be2efaa608f8e2b4b9d7b8a43d450
Alejandro Javier Schwoykoski <alejandros@arista.com>
2020-08-09 21:48:56 +1000

    completed run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
+source utils.sh
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
-
-# Step 2
+# Step 1
 # Run the Docker Hub container with kubernetes
+echo "Creating kubernetes service/pod $PROJECT"
+kubectl run $PROJECT \
+    --generator=run-pod/v1 \
+    --image=$dockerpath \
+    --port=$APP_PORT --labels app=$PROJECT \
+    --expose
 
+# Step 2:
+# List kubernetes pods
+kubectl get pods
+POD_NAME=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $POD_NAME
+
+# Wait for pod to be up
+echo "Waiting for $POD_NAME to be Ready"
+kubectl wait --for=condition=Ready pod/$POD_NAME
 
 # Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
-
+# Forward the container port to host
+echo "Forwarding application port $APP_PORT --> $SERVICE_PORT"
+kubectl port-forward $PROJECT $SERVICE_PORT:$APP_PORT
