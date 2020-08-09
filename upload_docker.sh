6bf80986b719ddf254b3f14e25872d12733d0296
Alejandro Javier Schwoykoski <alejandros@arista.com>
2020-08-09 12:42:12 +1000

    add upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
+source utils.sh
 
 # Assumes that an image is built via `run_docker.sh`
+if [ -z $(image-id) ]; then
+    error-exit "No image to upload, have you run `run_docker.sh`?"
+fi
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="${USER}/${PROJECT}"
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker-login
+docker tag image-id ${dockerpath}
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
